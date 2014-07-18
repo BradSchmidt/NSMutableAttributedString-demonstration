fe27befdd9c676d0cc9898ad2ef89a1aedc16680
BradSchmidt <brad@Bradleys-Mac-mini.local>
2014-07-17 21:57:21 -0700

    Initial Commit

---
+++ NSMutableAttributedStringExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NSMutableAttributedStringExample
+//
+//  Created by Bradley Robert Schmidt on 7/17/14.
+//  Copyright (c) 2014 Bradley Robert Schmidt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NSMutableAttributedStringExampleTests/NSMutableAttributedStringExampleTests.m
@@ -0,0 +1,34 @@
+//
+//  NSMutableAttributedStringExampleTests.m
+//  NSMutableAttributedStringExampleTests
+//
+//  Created by Bradley Robert Schmidt on 7/17/14.
+//  Copyright (c) 2014 Bradley Robert Schmidt. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NSMutableAttributedStringExampleTests : XCTestCase
+
+@end
+
+@implementation NSMutableAttributedStringExampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
